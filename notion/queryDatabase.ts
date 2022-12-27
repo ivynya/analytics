1d84fa3a7f495e96655af0b87c27fa2d0652df4c
ivynya <vyne@ivy.direct>
2022-12-27 13:39:26 -0800

    feat: Remove static file cache

--- notion/queryDatabase.ts
+++ notion/queryDatabase.ts
@@ -1,14 +1,7 @@
 
 import { flattenQuery } from "../deps.ts";
 
-const cachePath = `${Deno.cwd()}/notion/cache.json`;
-let lastUpdated = new Date(0);
-
 export async function queryDatabase(): Promise<any[]> {
-	if (lastUpdated.getTime() + 30000 > Date.now())
-		return JSON.parse(await Deno.readTextFile(cachePath));
-	else lastUpdated = new Date();
-	
 	const id = Deno.env.get("NOTION_DB_ID");
 	const res = await (await fetch(`https://api.notion.com/v1/databases/${id}/query`, {
 		method: "POST",
@@ -19,6 +12,5 @@ export async function queryDatabase(): Promise<any[]> {
 	})).json();
 	
 	const flatRes = flattenQuery(res);
-	await Deno.writeTextFile(cachePath, JSON.stringify(flatRes));
 	return flatRes;
 }
